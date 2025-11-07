4e64dd64bd83534666e822a3015873686744649c
Paulo <pauloboramos@hotmail.com>
2025-11-07 00:58:31 -0300

    fit: noticias

--- site-sprint3/src/pages/Noticias.jsx
+++ site-sprint3/src/pages/Noticias.jsx
@@ -1,8 +1,25 @@
+import { useState, useEffect } from "react";
+import CardNoticias from "../components/CardNoticias";
+
 export default function Noticias() {
-    return (
-        <div>
-            <h1>Notícias</h1>
-            <p>Bem-vindo à seção de notícias!</p>
-        </div>
-    )
+  const [noticias, setNoticias] = useState([]);
+
+    useEffect(() => {
+        fetch("https://690d6065a6d92d83e85145f4.mockapi.io/noticias/geral")
+        .then(response => response.json())
+        .then(data => setNoticias(data))
+        .catch(err => console.log(err))
+    }, [])
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 py-10 px-4">
+      <h1 className="text-4xl font-bold mb-10 text-purple-700">Notícias</h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full max-w-7xl">
+        {noticias.map((noticia, index) => (
+            <CardNoticias key={index} noticia={noticia} />
+        ))}
+      </div>
+    </div>
+  );
 }
